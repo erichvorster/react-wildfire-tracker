4218b49bbb366c1e94783f323f8de21a4e22fecf
erichvorster <erichvorster1@gmail.com>
2022-05-16 16:05:22 +0200

    1st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Map from "./components/Map";
+import { useState, useEffect } from "react";
+
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+};
+
+export default App;
